5b8939f60e048122cbafe8e781457af11f7da55e
John Corser <johnpc@umich.edu>
2024-05-21 00:00:24 -0700

    add ios build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: "com.johncorser.eats",
   appName: "jpc-eats",
   webDir: "dist",
+  ios: {
+    contentInset: "always",
+    backgroundColor: "#F5DEB3",
+  },
 };
 
 export default config;
