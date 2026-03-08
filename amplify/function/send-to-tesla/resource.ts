d4f53f7a8646c6161e35f958a27c9059780734d1
John Corser <johnpc@umich.edu>
2026-03-07 19:03:36 -0500

    Mark AWS SDK as external for Lambda bundling

--- amplify/function/send-to-tesla/resource.ts
+++ amplify/function/send-to-tesla/resource.ts
@@ -4,4 +4,7 @@ export const sendToTeslaFunction = defineFunction({
   entry: "./handler.ts",
   name: "sendToTesla",
   timeoutSeconds: 30,
+  bundling: {
+    externalModules: ["@aws-sdk/*"],
+  },
 });
